84e7680ff02f37f3aa75e09888c8ca29956c7b2a
Joy Wang <108701016+joyqvq@users.noreply.github.com>
2024-11-01 15:23:47 -0400

    lint

--- sui-execution/latest/sui-move-natives/src/crypto/attestation.rs
+++ sui-execution/latest/sui-move-natives/src/crypto/attestation.rs
@@ -29,7 +29,7 @@ pub fn nitro_attestation_verify(
     let enclave_pk = pop_arg!(args, VectorRef);
     let attestation = pop_arg!(args, VectorRef);
 
-   let attestation_ref = attestation.as_bytes_ref();
+    let attestation_ref = attestation.as_bytes_ref();
     let enclave_pk_ref = enclave_pk.as_bytes_ref();
     let pcr0_ref = pcr0.as_bytes_ref();
     let pcr1_ref = pcr1.as_bytes_ref();
