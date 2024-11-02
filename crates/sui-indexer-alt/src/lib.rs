1731ebabb5b0cae419ca0ef27b4c2dfea5ed6330
Ashok Menon <ashok@mystenlabs.com>
2024-11-02 01:18:58 +0000

    refactor(indexer-alt): configurable processor
    ## Description
    
    Change the pipeline/handler/processor interface to accept a value
    representing the handler/processor. This allows the processor to include
    configuration.
    
    This will be used by upcoming pipelines for processing protocol configs
    and feature flags.
    
    ## Test plan
    
    CI

--- crates/sui-indexer-alt/src/lib.rs
+++ crates/sui-indexer-alt/src/lib.rs
@@ -151,12 +151,16 @@ impl Indexer {
     /// Concurrent pipelines commit checkpoint data out-of-order to maximise throughput, and they
     /// keep the watermark table up-to-date with the highest point they can guarantee all data
     /// exists for, for their pipeline.
-    pub async fn concurrent_pipeline<H: concurrent::Handler + 'static>(&mut self) -> Result<()> {
+    pub async fn concurrent_pipeline<H: concurrent::Handler + Send + Sync + 'static>(
+        &mut self,
+        handler: H,
+    ) -> Result<()> {
         let Some(watermark) = self.add_pipeline::<H>().await? else {
             return Ok(());
         };
 
-        let (processor, collector, committer, watermark) = concurrent::pipeline::<H>(
+        let (processor, collector, committer, watermark) = concurrent::pipeline(
+            handler,
             watermark,
             self.pipeline_config.clone(),
             self.db.clone(),
@@ -183,8 +187,9 @@ impl Indexer {
     ///
     /// The pipeline can optionally be configured to lag behind the ingestion service by a fixed
     /// number of checkpoints (configured by `checkpoint_lag`).
-    pub async fn sequential_pipeline<H: sequential::Handler + 'static>(
+    pub async fn sequential_pipeline<H: sequential::Handler + Send + Sync + 'static>(
         &mut self,
+        handler: H,
         checkpoint_lag: Option<u64>,
     ) -> Result<()> {
         let Some(watermark) = self.add_pipeline::<H>().await? else {
@@ -193,7 +198,8 @@ impl Indexer {
 
         let (checkpoint_rx, watermark_tx) = self.ingestion_service.subscribe();
 
-        let (processor, committer) = sequential::pipeline::<H>(
+        let (processor, committer) = sequential::pipeline(
+            handler,
             watermark,
             self.pipeline_config.clone(),
             checkpoint_lag,
